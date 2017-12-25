ff8e84bf763404e9c6365e8a7640506484b1de0b
Artur Czajka <arturczajka@gmail.com>
2017-12-25 09:47:59 +0000

    Quickchecking sorting (#2)

--- exercise2/tests/Spec.hs
+++ exercise2/tests/Spec.hs
@@ -1,12 +1,29 @@
-module Spec where
-
 import           Data.List       (sort)
 import           Lib
 import           Test.QuickCheck
 
-main = quickCheck tests
+main = sequence_ tests
 
-tests = qsortIsSorting
+tests = [ longCheck qsortIsSorting
+        , longCheck quicksortIsSortingInts
+        , longCheck quicksortIsSortingDoubles
+        , longCheck quicksortIsSortingChars
+        , quickCheck quicksortIsSortingStrings
+        ]
+    where longCheck :: Testable prop => prop -> IO ()
+          longCheck = quickCheckWith stdArgs { maxSuccess = 1000 }
 
 qsortIsSorting :: [Int] -> Bool
-qsortIsSorting xs = undefined
+qsortIsSorting xs = qsort xs == sort xs
+
+quicksortIsSortingInts :: [Int] -> Bool
+quicksortIsSortingInts xs = quicksort xs == sort xs
+
+quicksortIsSortingDoubles :: [Double] -> Bool
+quicksortIsSortingDoubles xs = quicksort xs == sort xs
+
+quicksortIsSortingChars :: [Char] -> Bool
+quicksortIsSortingChars xs = quicksort xs == sort xs
+
+quicksortIsSortingStrings :: [String] -> Bool
+quicksortIsSortingStrings xs = quicksort xs == sort xs
